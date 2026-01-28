4dfb6152a55848d1255afe61efae5300de75171e
jwra <jackrains@pm.me>
2026-01-29 00:26:29 +0100

    Initial Commit

---
+++ PortLight/PortLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortLightApp.swift
+//  PortLight
+//
+//  Created by Jack Rains on 29.01.26.
+//
+
+import SwiftUI
+
+@main
+struct PortLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
